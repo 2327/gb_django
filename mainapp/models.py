36023c5e1093514804a52753b97d20f0d4a01d84
Maxim Klyuev <klyuevmv@mos.ru>
2019-02-11 15:58:22 +0300

    step7: fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,6 +6,7 @@ class Categories(models.Model):
     name         = models.CharField(verbose_name='имя', max_length=64, unique=True)
     href         = models.CharField(verbose_name='ссылка', max_length=64, unique=True)
     description  = models.TextField(verbose_name='описание', max_length=64, blank=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
 
     def __str__(self):
         return self.name
