6195f508d48bd84c0612cc2b3cbf405eecc9533e
Maxim Klyuev <klyuevmv@mos.ru>
2019-03-05 15:32:36 +0300

    step3: added additional properties of basket

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
 
     re_path(r'^admin/', include('adminapp.urls', namespace='admin')),
     re_path(r'', include('social_django.urls', namespace='social')),
+    re_path(r'^order/', include('ordersapp.urls', namespace='order')),
 ]
 
 if settings.DEBUG:
